60393c1379d69666d8e005493b5b07187a4afb4e
JosueSay <josuesay770@gmail.com>
2025-07-29 10:50:28 -0600

    feat: cambios para setear mensaje original - hamming

--- receptor/src/main/java/decoders/HammingDecoder.java
+++ receptor/src/main/java/decoders/HammingDecoder.java
@@ -6,13 +6,15 @@ import java.util.Map;
 public class HammingDecoder {
   // Entradas
   private String message;
-  private int dataBits;
-  private int totalBits;
+  private int bitsConfiguration;
   private boolean isExtended;
   private boolean isEvenParity;
+  private int dataBits;
+  private int totalBits;
 
   // Datos auxiliares
   private int redundancyBits;
+  private int maxPosition;
   private List<Integer> posRedundancyBits;
   private Map<Integer, List<Integer>> redundancyCoverageMap;
 
@@ -26,7 +28,105 @@ public class HammingDecoder {
 
   // Documentación
   private List<String> detailLines;
-  private List<String> typeBit = List.of("d", "r", "rg", "alg", "len");
+  private List<String> typeBit = List.of("d", "r", "rg", "alg", "conf");
+
+  /*
+   * Constructor de la clase Receiver
+   */
+  public HammingDecoder(String message, int bitsConfiguration, boolean isExtended, boolean isEvenParity) {
+    this.message = message;
+    this.bitsConfiguration = bitsConfiguration;
+    this.isExtended = isExtended;
+    this.isEvenParity = isEvenParity;
+    this.totalBits = message.length();
+
+    // datos calculados
+    this.dataBits = extractDataBits();
+    this.maxPosition = this.isExtended ? this.totalBits - 1 : this.totalBits;
+    this.redundancyBits = calculateRedundancyBits();
+    this.posRedundancyBits = calculatePositionRedundancyBits();
+
+  }
+
+  /**
+   * Obtener la cantidad de data bits dado el valor apartado para representar los
+   * bits de data
+   */
+  private int extractDataBits() {
+    String bits = message.substring(1, 1 + this.bitsConfiguration);
+    return Integer.parseInt(bits, 2);
+  }
+
+  /*
+   * Método para saber cuantos bits de redundancia vamos a tener
+   */
+  private int calculateRedundancyBits() {
+    int r = 0;
+    while (Math.pow(2, r) < (this.dataBits + r + 1)) {
+      r++;
+    }
+    return r;
+  }
+
+  /**
+   * Calcula y asigna las posiciones de los bits de redundancia (potencias de 2)
+   * según si el mensaje tiene paridad extendida o no.
+   */
+  private List<Integer> calculatePositionRedundancyBits() {
+    List<Integer> positions = new java.util.ArrayList<>();
+    int positionTemp = 0;
+
+    while (Math.pow(2, positionTemp) <= this.maxPosition) {
+      positions.add((int) Math.pow(2, positionTemp));
+      positionTemp++;
+    }
+
+    return positions;
+  }
+
+  /**
+   * Construye el mapeo de los bits recibidos, etiquetando cada uno según su rol:
+   * - "alg": algoritmo
+   * - "conf": configuración de longitud de datos (ej. 5 bits)
+   * - "r": bit de redundancia Hamming
+   * - "d": bit de dato
+   * - "rg": bit de paridad global (si aplica)
+   */
+  public void bitSetupReceived() {
+    this.detailLines = new java.util.ArrayList<>();
+    this.msgBitsReceived = new java.util.ArrayList<>();
+    this.detailLines.add("Construcción del mensaje recibido:");
+
+    for (int i = 1; i <= this.totalBits; i++) {
+      int bit = Character.getNumericValue(this.message.charAt(i - 1));
+      String tipo;
+
+      if (i == 1) {
+        tipo = this.typeBit.get(3); // "alg"
+        this.detailLines.add(String.format("\t- Posición %d: bit '%d' (tipo de algoritmo, alg)", i, bit));
+      } else if (i <= 1 + this.bitsConfiguration) {
+        tipo = this.typeBit.get(4); // "conf"
+        this.detailLines.add(String.format("\t- Posición %d: bit '%d' (configuración de longitud, conf)", i, bit));
+      } else if (this.isExtended && i == this.totalBits) {
+        tipo = this.typeBit.get(2); // "rg"
+        this.detailLines.add(String.format("\t- Posición %d: bit '%d' (paridad global, rg)", i, bit));
+      } else {
+        int logicalIndex = i - (1 + this.bitsConfiguration);
+
+        if (this.posRedundancyBits.contains(logicalIndex + 1)) {
+          tipo = this.typeBit.get(1); // "r"
+          this.detailLines.add(String.format("\t- Posición %d: bit '%d' (paridad Hamming, r)", i, bit));
+        } else {
+          tipo = this.typeBit.get(0); // "d"
+          this.detailLines.add(String.format("\t- Posición %d: bit '%d' (dato, d)", i, bit));
+        }
+      }
+
+      this.msgBitsReceived.add(new java.util.AbstractMap.SimpleEntry<>(bit, tipo));
+    }
+
+    this.detailLines.add("");
+  }
 
   /**
    * Obtener el mensaje binario recibido
@@ -37,6 +137,18 @@ public class HammingDecoder {
 
   public void setMessage(String message) {
     this.message = message;
+  }
+
+  /**
+   * Obtener la cantidad de bits apartados para representar la cantidad de bits de
+   * datos
+   */
+  public int getBitsConfiguration() {
+    return this.bitsConfiguration;
+  }
+
+  public void setBitsConfiguration(int bitsConfiguration) {
+    this.bitsConfiguration = bitsConfiguration;
   }
 
   /**
@@ -92,6 +204,18 @@ public class HammingDecoder {
 
   public void setRedundancyBits(int redundancyBits) {
     this.redundancyBits = redundancyBits;
+  }
+
+  /**
+   * Obtener la posición maxima de la cadena de bits (en caso de usar un bit de
+   * paridad global)
+   */
+  public int getMaxPosition() {
+    return this.maxPosition;
+  }
+
+  public void setMaxPosition(int maxPosition) {
+    this.maxPosition = maxPosition;
   }
 
   /**
